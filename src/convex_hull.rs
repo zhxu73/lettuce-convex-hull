817b6eefdaba78ab7fd696e1bc292d7240eb9aca
John Xu <zhxu73@email.arizona.edu>
2021-12-05 19:16:16 -0700

    add convex hull test case

--- src/convex_hull.rs
+++ src/convex_hull.rs
@@ -91,3 +91,136 @@ fn orientation(pt1: &Point, pt2: &Point, pt3: &Point) -> i32 {
     };
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::convex_hull::javis_march;
+    use crate::geometry::Point;
+
+    struct convex_hull_test_case {
+        data: Vec<Point>,
+        expected_hull: Vec<Point>,
+    }
+    fn all_convex_hull_test_cases() -> Vec<convex_hull_test_case> {
+        vec![
+            convex_hull_test_case {
+                data: vec![
+                    Point::new2d_int(262, -103),
+                    Point::new2d_int(93, 120),
+                    Point::new2d_int(162, 52),
+                    Point::new2d_int(126, -167),
+                    Point::new2d_int(-95, 140),
+                    Point::new2d_int(-78, -114),
+                ],
+                expected_hull: vec![
+                    Point::new2d_int(-95, 140),
+                    Point::new2d_int(93, 120),
+                    Point::new2d_int(162, 52),
+                    Point::new2d_int(262, -103),
+                    Point::new2d_int(126, -167),
+                    Point::new2d_int(-78, -114),
+                ],
+            },
+            convex_hull_test_case {
+                data: vec![
+                    Point::new2d_int(-279, -115),
+                    Point::new2d_int(-247, -41),
+                    Point::new2d_int(-158, -6),
+                    Point::new2d_int(-153, -68),
+                    Point::new2d_int(23, -4),
+                    Point::new2d_int(235, -130),
+                ],
+                expected_hull: vec![
+                    Point::new2d_int(-279, -115),
+                    Point::new2d_int(-247, -41),
+                    Point::new2d_int(-158, -6),
+                    Point::new2d_int(23, -4),
+                    Point::new2d_int(235, -130),
+                ],
+            },
+            convex_hull_test_case {
+                data: vec![
+                    Point::new2d_int(-270, 45),
+                    Point::new2d_int(-43, -38),
+                    Point::new2d_int(-10, 13),
+                    Point::new2d_int(126, -140),
+                    Point::new2d_int(156, 77),
+                    Point::new2d_int(181, -48),
+                ],
+                expected_hull: vec![
+                    Point::new2d_int(-270, 45),
+                    Point::new2d_int(156, 77),
+                    Point::new2d_int(181, -48),
+                    Point::new2d_int(126, -140),
+                ],
+            },
+            convex_hull_test_case {
+                data: vec![
+                    Point::new2d_int(-202, 85),
+                    Point::new2d_int(-99, 109),
+                    Point::new2d_int(-97, -116),
+                    Point::new2d_int(-54, 1),
+                    Point::new2d_int(34, 31),
+                    Point::new2d_int(40, -94),
+                    Point::new2d_int(77, 168),
+                    Point::new2d_int(81, 115),
+                    Point::new2d_int(175, 23),
+                    Point::new2d_int(232, 53),
+                    Point::new2d_int(237, -161),
+                    Point::new2d_int(243, -152),
+                ],
+                expected_hull: vec![
+                    Point::new2d_int(-202, 85),
+                    Point::new2d_int(77, 168),
+                    Point::new2d_int(232, 53),
+                    Point::new2d_int(243, -152),
+                    Point::new2d_int(237, -161),
+                    Point::new2d_int(-97, -116),
+                ],
+            },
+            convex_hull_test_case {
+                data: vec![
+                    Point::new2d_int(-21, -124),
+                    Point::new2d_int(272, 68),
+                    Point::new2d_int(-221, -174),
+                    Point::new2d_int(-212, 126),
+                    Point::new2d_int(-151, -23),
+                    Point::new2d_int(-58, 76),
+                    Point::new2d_int(-53, -81),
+                    Point::new2d_int(130, 114),
+                    Point::new2d_int(245, -24),
+                    Point::new2d_int(85, -40),
+                ],
+                expected_hull: vec![
+                    Point::new2d_int(-221, -174),
+                    Point::new2d_int(-212, 126),
+                    Point::new2d_int(130, 114),
+                    Point::new2d_int(272, 68),
+                    Point::new2d_int(245, -24),
+                    Point::new2d_int(-21, -124),
+                ],
+            },
+        ]
+    }
+
+    #[test]
+    fn test_javis_march() {
+        let all_test_cases = all_convex_hull_test_cases();
+
+        for (i, test_case) in all_test_cases.iter().enumerate() {
+            let hull = javis_march(&test_case.data);
+            println!("test_case {}", i);
+            assert_eq!(hull.len(), test_case.expected_hull.len());
+            for pt1 in &hull {
+                let mut found = false;
+                for pt2 in &test_case.expected_hull {
+                    if pt1 == pt2 {
+                        found = true;
+                        break;
+                    };
+                }
+                assert!(found);
+            }
+        }
+    }
+}
