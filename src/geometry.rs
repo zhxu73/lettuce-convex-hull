716b39eb92301adb8a48caf90b700ee24676a12f
John Xu <zhxu73@email.arizona.edu>
2021-12-07 11:11:00 -0700

    add more geometry type

--- src/geometry.rs
+++ src/geometry.rs
@@ -62,8 +62,55 @@ pub fn sort_points_by_y(data: &mut Vec<Point>) {
     data.sort_by(|pt1, pt2| pt1.y.partial_cmp(&pt2.y).unwrap());
 }
 
-#[allow(dead_code)]
-pub struct Line {
+#[derive(Copy, Clone, Serialize, Deserialize)]
+pub struct Triangle {
     pub p1: Point,
     pub p2: Point,
+    pub p3: Point,
+}
+impl Triangle {
+    pub fn new(p1: Point, p2: Point, p3: Point) -> Self {
+        Triangle { p1, p2, p3 }
+    }
+}
+
+pub fn triangles_to_pts(data: Vec<Triangle>) -> Vec<Point> {
+    let mut result = vec![];
+    result.reserve(data.len() * 3);
+    for tri in data {
+        result.push(tri.p1);
+        result.push(tri.p2);
+        result.push(tri.p3);
+    }
+    result
+}
+
+#[derive(Copy, Clone, Serialize, Deserialize)]
+pub struct Edge {
+    pub p1: Point,
+    pub p2: Point,
+}
+impl Edge {
+    pub fn new(p1: Point, p2: Point) -> Self {
+        Edge { p1, p2 }
+    }
+}
+
+impl PartialEq<Self> for Edge {
+    fn eq(&self, other: &Self) -> bool {
+        (self.p1 == other.p1 && self.p2 == other.p2) || (self.p2 == other.p1 && self.p1 == other.p2)
+    }
+}
+
+#[derive(Copy, Clone, Serialize, Deserialize)]
+pub struct Vec3D {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vec3D {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Vec3D { x, y, z }
+    }
 }
